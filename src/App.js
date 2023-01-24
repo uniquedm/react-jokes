3ee5976084da5e154740073868faa2612b047746
Devansh Mishra <devanshunique@gmail.com>
2023-01-24 23:45:30 +0530

    Mapping Components Data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Joke from './components/Jokes';
+import jokesData from './jokesData';
 
 function App() {
+  const render = jokesData.map((joke)=>{
+    const {setup, punchline} = joke
+    return <Joke setup={setup} punchline={punchline} />
+  })
   return (
     <div className="jokes-layout">
-      <Joke setup="How did the hacker escape the police?" punchline="He just ransomware!"/>
-      <Joke setup="Why don't pirates travel on mountain roads?" punchline="Scurvy."/>
-      <Joke setup="Why do bees stay in the hive in the winter?" punchline="Swarm."/>
-      <Joke setup="What's the best thing about Switzerland?" punchline="I don't know, but the flag is a big plus!"/>
+      {render}
     </div>
   );
 }
